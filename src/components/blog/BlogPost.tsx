787769262b292f2c73c881325abeb8abf7673cd5
Asher T Chiwashira <92381139+Tkdzw@users.noreply.github.com>
2025-03-03 14:46:21 +0200

    Update excerpt length and format in share message for BlogPost component

--- src/components/blog/BlogPost.tsx
+++ src/components/blog/BlogPost.tsx
@@ -98,10 +98,10 @@ const BlogPost: React.FC<BlogPostProps> = ({ post, onEdit }) => {
   
     // Extract a short excerpt from post.content
     const excerpt = post.content
-      ? post.content.replace(/<\/?[^>]+(>|$)/g, "").slice(0, 150) + "..." // Strips HTML tags and trims content
+      ? post.content.replace(/<\/?[^>]+(>|$)/g, "").slice(0, 500) + "..." // Strips HTML tags and trims content
       : "Check out this amazing blog post!";
   
-    const shareMessage = `📖 "${post.title}" – ${excerpt}\n🔗 Read more: ${shareUrl}`;
+    const shareMessage = `${post.title}\n – ${excerpt}\n🔗 Read more:`;
   
     if (navigator.share) {
       navigator.share({
